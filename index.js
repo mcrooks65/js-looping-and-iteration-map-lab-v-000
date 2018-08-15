877258a2aaaa79cb2a64db7c25d80d302e99562d
Matthew Crooks <mfcrooks@gmail.com>
2018-08-15 01:04:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(list) {
   return list.map(function (driver) {
-    reutnr driver.toLowerCase();
+    return driver.toLowerCase();
   }) ;
 }
